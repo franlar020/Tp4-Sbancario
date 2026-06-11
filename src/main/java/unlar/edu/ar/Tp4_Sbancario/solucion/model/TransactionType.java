9e646bd8a5c6e57bb4608d012da561175a860ec1
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-10 22:42:27 -0300

    Convertir TransactionType en un enum con tipos de transacción y descripción legible

--- src/main/java/unlar/edu/ar/Tp4_Sbancario/solucion/model/TransactionType.java
+++ src/main/java/unlar/edu/ar/Tp4_Sbancario/solucion/model/TransactionType.java
@@ -1,5 +1,21 @@
 package unlar.edu.ar.Tp4_Sbancario.solucion.model;
 
-public class TransactionType {
+public enum TransactionType {
 
+    DEPOSITO("Deposito"),
+    RETIRO("Retiro"),
+    TRANSFERENCIA("Transferencia"),
+    PAGO("Pago");
+
+    /** Descripción legible del tipo de transaccion. */
+    private final String descripcion;
+
+
+    TransactionType(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
